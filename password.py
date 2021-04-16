ab80c00afdd8216388f67d32e23ceb71fe9546f4
Dominic Hsiao <hsiaod@flip1.engr.oregonstate.edu>
2021-04-15 18:00:38 -0700

    added password generation functionality

--- password.py
+++ password.py
@@ -1,11 +1,12 @@
 import random
+import string
 
 def main():
 	password(7)
 
 def password(length):
 	for x in range (0, length):
-		pass
+		print (random.choice(string.ascii_letters), end = '')
 
 if __name__ == '__main__':
 	main()
